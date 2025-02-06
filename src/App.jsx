54b7237011568edeb9548a78d8d5fdb7aa6b3c11
Makc240305 <maxim240305@gmail.com>
2025-02-06 20:52:46 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./index.scss";
+import { Users } from "./Users/Index.jsx";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Users />
+      </div>
+    </>
+  );
+}
+
+export default App;
