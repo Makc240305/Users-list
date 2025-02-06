f6c34852f78963400a3e349da9c40defec777caf
Makc240305 <maxim240305@gmail.com>
2025-02-06 21:51:45 +0100

    Added some logic to react and style to scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useEffect, useState } from "react";
 import "./index.scss";
 import { Users } from "./Users/Index.jsx";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((result) => result.json())
+      .then((json) => setUsers(json.data))
+      .catch(erorr =>{
+        console.warn(erorr);
+        alert('Erorr with users')
+      })
+  }, []);
+
   return (
-    <>
-      <div className="App">
-        <Users />
-      </div>
-    </>
+    <div className="App">
+      <Users />
+      <button className="send-invite-btn">Send proposition</button>
+    </div>
   );
 }
 
