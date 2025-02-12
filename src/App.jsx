98148c00ee8686ed2f35a1106b95d7b939de6fa6
Makc240305 <maxim240305@gmail.com>
2025-02-12 23:37:45 +0100

    Added logic to react and styles to scss

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,32 @@ import { Users } from "./Users/Index.jsx";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
     fetch("https://reqres.in/api/users")
       .then((result) => result.json())
       .then((json) => setUsers(json.data))
-      .catch(erorr =>{
+      .catch((erorr) => {
         console.warn(erorr);
-        alert('Erorr with users')
+        alert("Erorr with users");
       })
+      .finally(() => setLoading(false));
   }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
 
   return (
     <div className="App">
-      <Users />
+      <Users
+        onChangeSearchValue={onChangeSearchValue}
+        searchValue={searchValue}
+        items={users}
+        isLoading={isLoading}
+      />
       <button className="send-invite-btn">Send proposition</button>
     </div>
   );
